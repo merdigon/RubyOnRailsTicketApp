762a37233b08285e953f4ffe77ba98a7d738c6b2
merdigon <szymon.nowak3848@gmail.com>
2018-01-14 00:51:59 +0100

    Lab10

---
+++ bilety/app/models/ticket.rb
@@ -0,0 +1,6 @@
+class Ticket < ApplicationRecord
+    validates :name, :presence => true, :length => { :minimum => 6 }
+    validates :email_address, :presence => true
+    validates :price, :presence => true
+    validates :phone, :presence => true
+end
